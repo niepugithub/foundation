638582b5c8d8e0a7ce1ada13f9307b54f6ffd0a6
niepu <niepu1991@163.com>
2020-09-03 20:54:49 +0800

    pipeline待定

---
+++ src/main/java/com/pattern/chain/pipeline/Pipeline.java
@@ -0,0 +1,7 @@
+package com.pattern.chain.pipeline;
+
+/**
+ * @date:2020/9/2 22:47
+ **/
+public interface Pipeline {
+}
---
+++ src/main/java/com/pattern/chain/pipeline/SimplePipeline.java
@@ -0,0 +1,7 @@
+package com.pattern.chain.pipeline;
+
+/**
+ * @date:2020/9/2 22:48
+ **/
+public class SimplePipeline {
+}
